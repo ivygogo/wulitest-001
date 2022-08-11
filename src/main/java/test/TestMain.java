58c66a0ea2a4f1246022e657abb1241a88a802b7
Sisoll <104013838+Sisoll@users.noreply.github.com>
2022-08-12 02:10:41 +0800

    Test for ci (#6)
    * 新增檔案用來測試CI是否正常運作
    
    * 新增用來測試CI的CLASS
    
    * 新增CI用的CLASS

---
+++ src/main/java/test/TestMain.java
@@ -0,0 +1,15 @@
+package test;
+
+
+
+public class TestMain {
+
+  public static void main(String[] args) {
+Test test = new Test();
+
+test.name = "Snoopy";
+test.age =18;
+
+    System.out.println(test.name + "'s age is "+ test.age);
+  }
+}
